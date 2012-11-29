a941bcef4880ce2c3b2df13b6469c728e3ce640d
John Chesley <john@chesl.es>
2012-11-29 10:59:52 -0700

    make test for #221 run under node.js

--- tests/test.issue221.js
+++ tests/test.issue221.js
@@ -1,55 +1,75 @@
-var qunit = module;
+var adapters = [
+    ['idb-1', 'http-1']
+  ]
+  , qunit = module;
 
-qunit('test for issue 221', {
-  setup: function() {
-    this.local = generateAdapterUrl('idb-1');
-    this.remote = generateAdapterUrl('http-1');
+if (typeof module !== undefined && module.exports) {
+  this.Pouch = require('../src/pouch.js');
+  this.LevelPouch = require('../src/adapters/pouch.leveldb.js');
+  this.utils = require('./test.utils.js');
+
+  for (var k in this.utils) {
+    global[k] = global[k] || this.utils[k];
   }
-});
+  adapters = [
+    ['leveldb-1', 'http-1']
+  ]
+  qunit = QUnit.module;
+}
 
-var doc = { _id: '0', integer: 0 };
+adapters.map(function(adapters) {
 
-asyncTest('Testing issue 221', function() {
-  var self = this;
-  // Create databases.
-  initDBPair(this.local, this.remote, function(local, remote) {
-    // Write a doc in CouchDB.
-    remote.put(doc, {}, function(err, results) {
-      // Update the doc.
-      doc._rev = results.rev;
-      doc.integer = 1;
+  qunit('test for issue 221', {
+    setup: function() {
+      this.local = generateAdapterUrl(adapters[0]);
+      this.remote = generateAdapterUrl(adapters[1]);
+    }
+  });
+
+  var doc = { _id: '0', integer: 0 };
+
+  asyncTest('Testing issue 221', function() {
+    var self = this;
+    // Create databases.
+    initDBPair(this.local, this.remote, function(local, remote) {
+      // Write a doc in CouchDB.
       remote.put(doc, {}, function(err, results) {
-        // Compact the db.
-        $.ajax({
-          url: self.remote + '/_compact',
-          type: 'POST',
-          contentType: 'application/json',
-          success: function(data, status, jqXHR) {
-            // Wait until compaction has affected the doc.
-            var checkDoc = function() {
-              $.ajax({
-                url: self.remote + '/' + doc._id + '?revs_info=true',
-                dataType: 'json',
-                success: function(data, status, jqXHR) {
-                  var correctRev = data._revs_info[0];
-                  if (data._revs_info[1].status == 'missing') {
-                    clearInterval(interval);
-                    // Replicate to PouchDB.
-                    local.replicate.from(remote, function(err, results) {
-                      // Check the PouchDB doc.
-                      local.get(doc._id, function(err, results) {
-                        ok(results._rev == correctRev, 'correct rev stored after replication');
-                        ok(results.integer == 1, 'correct content stored after replication');
-                        start();
+        // Update the doc.
+        doc._rev = results.rev;
+        doc.integer = 1;
+        remote.put(doc, {}, function(err, results) {
+          // Compact the db.
+          Pouch.utils.ajax({
+            url: self.remote + '/_compact',
+            type: 'POST',
+            contentType: 'application/json',
+            success: function(data, status, jqXHR) {
+              // Wait until compaction has affected the doc.
+              var checkDoc = function() {
+                Pouch.utils.ajax({
+                  url: self.remote + '/' + doc._id + '?revs_info=true',
+                  dataType: 'json',
+                  success: function(data, status, jqXHR) {
+                    var correctRev = data._revs_info[0];
+                    if (data._revs_info[1].status == 'missing') {
+                      clearInterval(interval);
+                      // Replicate to PouchDB.
+                      local.replicate.from(remote, function(err, results) {
+                        // Check the PouchDB doc.
+                        local.get(doc._id, function(err, results) {
+                          ok(results._rev == correctRev, 'correct rev stored after replication');
+                          ok(results.integer == 1, 'correct content stored after replication');
+                          start();
+                        });
                       });
-                    });
+                    }
                   }
-                }
-              });
-            };
-            console.log('setting');
-            var interval = setInterval(checkDoc, 100);
-          }
+                });
+              };
+              console.log('setting');
+              var interval = setInterval(checkDoc, 100);
+            }
+          });
         });
       });
     });
